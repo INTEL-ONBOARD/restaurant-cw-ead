45223d11817d6bc585941e9863998cd376080502
Rowell2002 <chethanarowell2002@gmail.com>
2024-09-15 02:55:42 +0530

    stock letter colour fix

--- src/Pages/layout.jsx
+++ src/Pages/layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
     <div className="flex h-screen bg-gray-100">
       <div className="w-64 bg-gray-800 text-black flex flex-col">
         <div className="text-center text-2xl font-semibold p-4">
-          <span className="text-yellow-400">Store Name</span>
+          <span className="text-yellow-400">Leornado</span>
         </div>
         <nav className="flex-grow">
           <ul className="space-y-2">
